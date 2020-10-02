6671eddf1d4409b626f29aad57bad5f8dd374e68
Jagmohan Rawat <jagmohan.rawat2020@gmail.com>
2020-10-02 11:35:13 +0530

    Initial Commit

---
+++ src/test/java/com/thportal/configserver/ThehopeConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thportal.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThehopeConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
